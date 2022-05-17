b7456ae746f09ee4276c5a37eb3ac5210eb7c0f2
Mathijs Wagemaker <matwagem@gmail.com>
2022-05-17 01:53:14 +0100

    Chirper commit

---
+++ cd0547-react-chirper-app/src/actions/shared.js
@@ -0,0 +1,20 @@
+import { getInitialData } from "../utils/api";
+import { receiveTweets } from "./tweets";
+import { receiveUsers } from "./users";
+import { setAuthedUser } from "./authedUser";
+import { showLoading, hideLoading } from "react-redux-loading-bar";
+
+const AUTHED_ID = "tylermcginnis";
+
+//Action to get the initial data
+export function handleInitialData(){
+    return(dispatch)=>{
+        dispatch(showLoading());
+        return getInitialData().then(({users, tweets})=>{
+            dispatch(receiveUsers(users));
+            dispatch(receiveTweets(tweets));
+            dispatch(setAuthedUser(AUTHED_ID));
+            dispatch(hideLoading());
+        })
+    }
+}
---
+++ cd0547-react-chirper-app/src/components/NewTweet.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { connect } from "react-redux";
+import { handleAddTweet } from "../actions/tweets";
+import { useNavigate } from "react-router-dom";
+
+const NewTweet = ({dispatch, id}) => {
+    const navigate = useNavigate();
+    const [text, setText] = useState('');
+
+    const handleChange = (e) => {
+        const text = e.target.value;
+        setText(text);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        dispatch(handleAddTweet(text, id));
+        setText("");
+        if(!id){
+            navigate("/")
+        } else {
+            //DO NOTHING, Customer already on tweet page, re-renders with new tweet
+        }
+    };
+
+    const tweetLeft = 280 - text.length;
+
+    return (
+        <div>
+            <h3 className="center">Compose new Tweet</h3>
+            <form className="new-tweet" onSubmit={handleSubmit}>
+                {/* TODO: Redirect to / once submitted */}
+                <textarea 
+                    className="textarea" 
+                    maxLength={280} 
+                    placeholder="What's happening?" 
+                    value={text} 
+                    onChange={handleChange}/>
+                {
+                    tweetLeft <= 100 && <div className="tweet-length">{tweetLeft}</div>
+                }
+                <button className="btn" type="submit" disabled={text === ""}>Submit</button>
+            </form>
+        </div>
+    )
+};
+
+export default connect()(NewTweet);
